f0865039407f6c0a0dc69c02d0eeeed2e6147096
tinystepstt-dot <tinystepstt@gmail.com>
2026-05-31 20:57:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -493,18 +493,7 @@ const convertVideoToSticker = async (videoBuffer) => {
   }
 };
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
-const askQuestion = (question) => {
-  return new Promise((resolve) => {
-    rl.question(question, (answer) => {
-      resolve(answer);
-    });
-  });
-};
 
 // Anonymous messaging helper functions
 const generateSessionId = () => {
